289fb8375d1158d5c8d38bd7494dbfcb782a1f32
armadon <armando.gavrila@gmail.com>
2021-06-26 14:12:47 +0300

    added list items

--- src/assets/Components/UI/Nav/Drawer/MenuDrawer/MenuDrawer.js
+++ src/assets/Components/UI/Nav/Drawer/MenuDrawer/MenuDrawer.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Drawer from '@material-ui/core/Drawer'
+import {Close} from "@material-ui/icons";
+import classes from './MenuDrawer.css';
+import {List, ListItem} from "@material-ui/core";
 
 const MenuDrawer = (props) => {
+    const listItems = ['contact', 'call me'];
+
     return (
         <Drawer open={props.open} onClose={props.toggleDrawer}>
-            <h5>Contact</h5>
-            <h5>Call me</h5>
-            <h5 onClick={props.toggleDrawer}>Close</h5>
+            <List>
+                {listItems.map(item => {
+                    return (
+                        <ListItem className={classes.link} onClick={props.item}>
+                            {item}
+                        </ListItem>
+                    );
+                })}
+                <ListItem className={classes.link} onClick={props.toggleDrawer}>
+                    <Close/>
+                    <h5>Close</h5>
+                </ListItem>
+            </List>
         </Drawer>
     );
 }
